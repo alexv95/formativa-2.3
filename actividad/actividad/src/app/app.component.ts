a8a647dd03592c438ac12b69925c3897bd110e71
DanSotoC <danielsotocorrea21@hotmail.com>
2021-04-22 12:47:02 -0400

    Formulario con Validacion

--- actividad/actividad/src/app/app.component.ts
+++ actividad/actividad/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'actividad';
+  datos: string = "";
+
+  formulario = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('',Validators.required),
+    telephone: new FormControl('',Validators.pattern("^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$")),
+    
+  });
+
+  metodo(){
+    this.datos = `Nombre=${this.formulario.value.name}
+                  Telefono=${this.formulario.value.telephone}
+                  Correo=${this.formulario.value.email}
+                  `;
+  }
 }
