1638e22e10249b66e52f06e055ce36a248c95098
niqart <nlaraya@uc.cl>
2021-04-22 14:37:34 -0400

    estilos de validacion y mejor validacion

--- actividad/actividad/src/app/app.component.ts
+++ actividad/actividad/src/app/app.component.ts
@@ -11,17 +11,24 @@ export class AppComponent {
   usuario !: Usuario;
   formulario = new FormGroup({
     name: new FormControl('', Validators.required),
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
     telephone: new FormControl('', [Validators.required, Validators.pattern('(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]\\d{3}[\\s.-]\\d{4}')]),
 
   });
 
   metodo() {
-    this.datos = `Nombre=${this.formulario.value.name}
-                  Telefono=${this.formulario.value.telephone}
-                  Correo=${this.formulario.value.email}
-                  `;
-    this.usuario = new Usuario(this.formulario.value.name, this.formulario.value.email, this.formulario.value.telephone);
+    if (this.formulario.value.name != "" && this.formulario.value.telephone != "" && this.formulario.value.email != "") {
+      if (this.formulario.value.name.length >= 4 && this.formulario.value.name.length <= 10) {
+        this.datos = `Nombre=${this.formulario.value.name}
+      Telefono=${this.formulario.value.telephone}
+      Correo=${this.formulario.value.email}
+      `;
+        this.usuario = new Usuario(this.formulario.value.name, this.formulario.value.email, this.formulario.value.telephone);
+      }
+    }
+    else {
+      alert("Complete todos los campos!");
+    }
   }
 }
 
